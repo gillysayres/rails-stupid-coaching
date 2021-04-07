cc164ad2e0c0216309e8b595ba9cbcaa91ad127e
Gillys Dantas <gillys.ayres@gmail.com>
2021-04-07 12:14:46 -0300

    Fixed wrong route and removed unecessary controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,7 @@
 class QuestionsController < ApplicationController
   def ask
   end
+
+  def answer
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # verb 'path', to: 'controller#action'
   get 'ask', to: 'questions#ask'
+  get 'answer', to: 'questions#answer'
 end
