e1ac7b5b2f77b05dfb4fed99e8b98afa309823c3
Gillys Dantas <gillys.ayres@gmail.com>
2021-04-08 19:07:24 -0300

    Fixed routing error

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,29 @@
 class QuestionsController < ApplicationController
   def ask
+    #   @user_answer = params[:question]
+    # if @user_answer
+    #   @user_answer = "well done!"
+    # else
+    #   @user_answer = "Type something!"
+    # end
   end
 
   def answer
+    @user_answer = params[:question]
+    # if @user_answer
+    #   @user_answer = "well done!"
+    # else
+    #   @user_answer = "Type something!"
+    # end
+    @user_answer
+  end
+
+  def home
+    # @user_answer = params[:question]
+    #   if @user_answer
+    #     @user_answer = "well done!"
+    #   else
+    #     @user_answer = "Type something!"
+    #   end
   end
 end
