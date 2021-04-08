c8d4db76b31a5e1117de6aa13ecb973c8ecee8a9
Gillys Dantas <gillys.ayres@gmail.com>
2021-04-08 19:10:35 -0300

    Erased misplaced functions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,5 @@
 class QuestionsController < ApplicationController
   def ask
-    #   @user_answer = params[:question]
-    # if @user_answer
-    #   @user_answer = "well done!"
-    # else
-    #   @user_answer = "Type something!"
-    # end
   end
 
   def answer
@@ -19,11 +13,5 @@ class QuestionsController < ApplicationController
   end
 
   def home
-    # @user_answer = params[:question]
-    #   if @user_answer
-    #     @user_answer = "well done!"
-    #   else
-    #     @user_answer = "Type something!"
-    #   end
   end
 end
